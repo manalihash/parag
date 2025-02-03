aae711bd774b9e7313e908c171bfd198b861b762
roammate-in <parag@roammate.in>
2025-02-03 17:00:43 +0530

    modifying 100

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
+# 
+
+#!/bin/bash
 set -e
 
-
-
+# List running containers
 docker ps
-docker stop paragpatil01/hello-world
-docker rm paragpatil01/hello-world
-docker rmi paragpatil01/hello-world
-#pull the docker image from docker hub
+
+# Stop & remove the container if it exists
+CONTAINER_ID=$(docker ps -aq --filter "ancestor=paragpatil01/hello-world")
+
+if [ ! -z "$CONTAINER_ID" ]; then
+    echo "Stopping existing container..."
+    docker stop "$CONTAINER_ID"
+    docker rm "$CONTAINER_ID"
+fi
+
+# Remove the image if it exists
+if docker images -q paragpatil01/hello-world; then
+    docker rmi paragpatil01/hello-world
+fi
+
+# Pull the latest image from Docker Hub
 docker pull paragpatil01/hello-world
 
-#run the docker image as a container 
-docker run -d -p 5000:5000 paragpatil01/hello-world
+# Run the container with a fixed name
+docker run -d -p 5000:5000 --name hello-world-container paragpatil01/hello-world
+
