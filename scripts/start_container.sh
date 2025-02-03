a1597eec2c2061b7d149c22e42149675e8aadcab
roammate-in <parag@roammate.in>
2025-02-03 17:12:58 +0530

    modifying 32525

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -7,10 +7,10 @@ set -e
 # List running containers
 docker ps
 
-sudo docker ps
-sudo docker stop paragpatil01/hello-world
-sudo docker rmi paragpatil01/hello-world
-# Pull the latest image from Docker Hub
+# sudo docker ps
+# sudo docker stop paragpatil01/hello-world
+# sudo docker rmi paragpatil01/hello-world
+# # Pull the latest image from Docker Hub
 docker pull paragpatil01/hello-world
 
 # Run the container with a fixed name
