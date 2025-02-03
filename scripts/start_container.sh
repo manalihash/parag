82fa8931b821dc303d86d0ffdad91f81f3995fa2
roammate-in <parag@roammate.in>
2025-02-03 13:05:08 +0530

    adding files

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 #pull the docker image from docker hub
-docker pull paragpatil01/simple-pthon-flask-app
+docker pull paragpatil01/hello-world
 
 #run the docker image as a container 
-docker run -d -p 5000:5000 paragpatil01/simple-pthon-flask-app
+docker run -d -p 5000:5000 paragpatil01/hello-world
