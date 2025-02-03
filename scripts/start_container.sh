daaaa3123163560152f24337bbcd9194274047e7
roammate-in <parag@roammate.in>
2025-02-03 16:52:09 +0530

    modifying 55

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+
+
+docker ps
+docker stop paragpatil01/hello-world
+docker rm paragpatil01/hello-world
+docker rmi paragpatil01/hello-world
 #pull the docker image from docker hub
 docker pull paragpatil01/hello-world
 
