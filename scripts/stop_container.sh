f1e2e5854fbde6a4020e731e9615be713623b7cd
roammate-in <parag@roammate.in>
2025-02-03 15:28:18 +0530

    modifying

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-set -e
+set -x
 
 #strop the container (if any running)
 # containerid=`docker ps | awk -F " " '{print $1}'`
 # docker rm -f $containerid
 
-sudo docker ps
-sudo docker stop paragpatil01/hello-world
-sudo docker rmi paragpatil01/hello-world
+# sudo docker ps
+# sudo docker stop paragpatil01/hello-world
+# sudo docker rmi paragpatil01/hello-world
+
+echo "hello "
